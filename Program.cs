ecc87899633fe69526a1d36dea8a57087447f421
Aref Karimi <aref.karimi@just-eat.com>
2018-01-07 19:26:59 +1100

    Initial add

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StatsDClient
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while(true)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var statsDClient = new StatsD("graphite.learngraphana.com");
+           
+                    statsDClient.Send("SqlServer.Orders.LastMinute:1|g");
+}
+                System.Threading.Thread.Sleep(500);
+                Console.WriteLine("Metric was sent!");
+            }
+        }
+    }
+}
---
+++ StatsD.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+      
+namespace StatsDClient
+{
+    public class StatsD
+    {
+        private StatsD()
+        {
+            
+        }
+
+        private string host = "";
+
+        public StatsD(string _host)
+
+        {
+            host = _host;
+        }
+
+		public void Send(string dataGram)
+		{
+            using (var socket = new System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Dgram, System.Net.Sockets.ProtocolType.Udp)) 
+            {
+                var serverAddress = Dns.GetHostEntry(host);
+                var endPoint = new IPEndPoint(serverAddress.AddressList[0], 8125);
+                var bytes = System.Text.Encoding.UTF8.GetBytes(dataGram);
+                socket.SendTo(bytes, endPoint);
+            }
+		}
+    }
+}

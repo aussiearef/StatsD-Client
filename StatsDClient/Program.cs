5e9f718d5099cb419d75023e0a4e18f5e5dbd49a
Aref Karimi <aref.karimi@just-eat.com>
2018-01-07 19:30:11 +1100

    Reorganised files

--- StatsDClient/Program.cs
+++ StatsDClient/Program.cs
@@ -6,7 +6,7 @@ namespace StatsDClient
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sending metrics to StatsD");
 
             while(true)
             {
